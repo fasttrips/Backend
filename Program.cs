b9b512b91aa225bce165d3e21c6e019bcba0953f
hilmanzu <hilmanzutech@gmail.com>
2025-04-19 10:06:37 +0700

    feat:

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@ builder.Services.AddAuthentication(options =>
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ABI", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "trasgo", Version = "v1" });
     c.OperationFilter<SwaggerFileOperationFilter>();
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
